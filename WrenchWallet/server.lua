ede860910a698dd02e007d5e78fe6bd4b78de0b2
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2024-04-11 21:27:42 -0500

    Update server.lua

--- WrenchWallet/server.lua
+++ WrenchWallet/server.lua
@@ -10,18 +10,20 @@ end)
 
 
 RegisterNetEvent("WrenchWallet:RegisterWallet", function(plrid)
-    local identifier = GetPlayerIdentifier(plrid, 1)
-    local specid = tostring(math.random(1,1000000))
-    local metadata = {walletid2 = (identifier .. specid), label = "Wallet", image = "wrenchwallet"}
-    exports.ox_inventory:AddItem(plrid, "wrenchwallet", 1, metadata)
-    if not exports.ox_inventory:GetInventory(tostring(identifier .. specid)) then
-        local stash = {
-            id = tostring(identifier .. specid),
-            label = 'Wallet',
-            slots = 5,
-            weight = 2000,
-            owner = false
-        }
-        exports.ox_inventory:RegisterStash(stash.id, stash.label, stash.slots, stash.weight, stash.owner)
+    if exports.ox_inventory:GetItemCount(plrid, "wrenchwallet") == 0 then
+        local identifier = GetPlayerIdentifier(plrid, 1)
+        local specid = tostring(math.random(1,1000000))
+        local metadata = {walletid2 = (identifier .. specid), label = "Wallet", image = "wrenchwallet"}
+        exports.ox_inventory:AddItem(plrid, "wrenchwallet", 1, metadata)
+        if not exports.ox_inventory:GetInventory(tostring(identifier .. specid)) then
+            local stash = {
+                id = tostring(identifier .. specid),
+                label = 'Wallet',
+                slots = 5,
+                weight = 2000,
+                owner = false
+            }
+            exports.ox_inventory:RegisterStash(stash.id, stash.label, stash.slots, stash.weight, stash.owner)
+        end
     end
 end)
